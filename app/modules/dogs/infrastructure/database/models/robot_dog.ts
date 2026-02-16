41fc229958b65c975c253615a5eb52b92385c43a
Fantom <arthur.morelon@gmail.com>
2026-02-16 17:58:45 +0100

    add robotDog lucid Model

---
+++ app/modules/dogs/infrastructure/database/models/robot_dog.ts
@@ -0,0 +1,40 @@
+import { DateTime } from 'luxon'
+import { BaseModel, beforeCreate, column } from '@adonisjs/lucid/orm'
+import { RobotDogState } from '../../../domain/enums/robot_dog.state.js'
+import { randomUUID } from 'node:crypto'
+
+export default class RobotDog extends BaseModel {
+  public static table = 'robot_dogs'
+
+  @column({ isPrimary: true })
+  declare id: string
+
+  @column()
+  declare serialNumber: string
+
+  @column()
+  declare key: string
+
+  @column()
+  declare name: string
+
+  @column()
+  declare state: RobotDogState
+
+  @column()
+  declare batteryLevel: number
+
+  @column.dateTime({ autoCreate: false, autoUpdate: false })
+  declare lastHeartbeat?: DateTime
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @beforeCreate()
+  static assignUuid(robotDog: RobotDog) {
+    robotDog.id = randomUUID()
+  }
+}
