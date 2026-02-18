0c8379c76b7623e9c85d3a42fb9b0dfb14250c4d
Fantom <arthur.morelon@gmail.com>
2026-02-18 22:27:37 +0100

    improve index_robot_dogs.spec.ts

--- tests/unit/dogs/application/usecases/index_robot_dogs.spec.ts
+++ tests/unit/dogs/application/usecases/index_robot_dogs.spec.ts
@@ -1,45 +1,24 @@
 import { test } from '@japa/runner'
-import { RobotDog } from '../../../../../app/modules/dogs/domain/robot_dog.entity.js'
+import { FakeRobotDogRepository } from '#tests/unit/fakes/fake_robot_dog_repository'
 import { ListRobotDogsUseCase } from '../../../../../app/modules/dogs/application/usecases/index-robot-dogs.use-case.js'
-import { RobotDogRepository } from '../../../../../app/modules/dogs/domain/contracts/robot_dog.repository.js'
+import { RobotDog } from '../../../../../app/modules/dogs/domain/robot_dog.entity.js'
 
-class FakeRobotDogRepository implements RobotDogRepository {
-  private dogs: RobotDog[] = []
 
-  async findById(id: any) {
-    return this.dogs.find((dog) => dog.id.equals(id)) ?? null
-  }
+test.group('ListRobotDogsUseCase', (group) => {
+  let fakeRepo: FakeRobotDogRepository
+  let useCase: ListRobotDogsUseCase
 
-  async findAll() {
-    return this.dogs
-  }
+  group.each.setup(() => {
+    fakeRepo = new FakeRobotDogRepository()
+    useCase = new ListRobotDogsUseCase(fakeRepo)
+  })
 
-  async save(dog: RobotDog) {
-    const index = this.dogs.findIndex((d) => d.id.equals(dog.id))
-
-    if (index >= 0) {
-      this.dogs[index] = dog
-    } else {
-      this.dogs.push(dog)
-    }
-  }
-
-  async delete(id: any) {
-    this.dogs = this.dogs.filter((dog) => !dog.id.equals(id))
-  }
-}
-
-test.group('Dogs application usecases list robot dogs', () => {
   test('should return all robot dogs', async ({ assert }) => {
-    const fakeRepo = new FakeRobotDogRepository()
-
     const dog1 = RobotDog.create('SN-001', 'Rex', 80)
     const dog2 = RobotDog.create('SN-002', 'Bolt', 70)
 
     await fakeRepo.save(dog1)
     await fakeRepo.save(dog2)
-
-    const useCase = new ListRobotDogsUseCase(fakeRepo)
 
     const result = await useCase.execute()
 
@@ -49,9 +28,6 @@ test.group('Dogs application usecases list robot dogs', () => {
   })
 
   test('should return empty array if no robot dogs', async ({ assert }) => {
-    const fakeRepo = new FakeRobotDogRepository()
-    const useCase = new ListRobotDogsUseCase(fakeRepo)
-
     const result = await useCase.execute()
 
     assert.lengthOf(result, 0)
