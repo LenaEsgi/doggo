8ccb45c410177c6c869ba4858cbfc876383cf17d
Fantom <arthur.morelon@gmail.com>
2026-02-18 19:21:38 +0100

    remplace robot_dog.repository.ts interface by abstract class for DI

--- app/modules/dogs/domain/contracts/robot_dog.repository.ts
+++ app/modules/dogs/domain/contracts/robot_dog.repository.ts
@@ -1,9 +1,9 @@
 import { RobotDog } from '../robot_dog.entity.js'
 import { RobotDogId } from '../value-objects/robot-dog-id.js'
 
-export interface RobotDogRepository {
-  findById(id: RobotDogId): Promise<RobotDog | null>
-  findAll(): Promise<RobotDog[]>
-  save(dog: RobotDog): Promise<void>
-  delete(id: RobotDogId): Promise<void>
+export abstract class RobotDogRepository {
+  abstract findById(id: RobotDogId): Promise<RobotDog | null>
+  abstract findAll(): Promise<RobotDog[]>
+  abstract save(dog: RobotDog): Promise<void>
+  abstract delete(id: RobotDogId): Promise<void>
 }
