50ca8883155b2bc8660e759b0e108de10db348ac
Fantom <arthur.morelon@gmail.com>
2026-02-18 22:31:14 +0100

    improve update_robot_dog.spec.ts

--- tests/unit/dogs/application/usecases/update_robot_dog.spec.ts
+++ tests/unit/dogs/application/usecases/update_robot_dog.spec.ts
@@ -1,44 +1,24 @@
 import { test } from '@japa/runner'
-import { RobotDog } from '../../../../../app/modules/dogs/domain/robot_dog.entity.js'
+import { FakeRobotDogRepository } from '#tests/unit/fakes/fake_robot_dog_repository'
 import {
   UpdateRobotDogUseCase
 } from '../../../../../app/modules/dogs/application/usecases/update-robot-dog.use-case.js'
-import { RobotDogRepository } from '../../../../../app/modules/dogs/domain/contracts/robot_dog.repository.js'
+import { RobotDog } from '../../../../../app/modules/dogs/domain/robot_dog.entity.js'
 import { RobotDogNotFoundError } from '../../../../../app/modules/dogs/domain/exceptions/robot-dog-not-found.error.js'
 
-class FakeRobotDogRepository implements RobotDogRepository {
-  private dogs: RobotDog[] = []
+test.group('UpdateRobotDogUseCase', (group) => {
+  let fakeRepo: FakeRobotDogRepository
+  let useCase: UpdateRobotDogUseCase
 
-  async findById(id: any) {
-    return this.dogs.find((dog) => dog.id.equals(id)) ?? null
-  }
+  group.each.setup(() => {
+    fakeRepo = new FakeRobotDogRepository()
+    useCase = new UpdateRobotDogUseCase(fakeRepo)
+  })
 
-  async findAll() {
-    return this.dogs
-  }
-
-  async save(dog: RobotDog) {
-    const index = this.dogs.findIndex((d) => d.id.equals(dog.id))
-
-    if (index >= 0) {
-      this.dogs[index] = dog
-    } else {
-      this.dogs.push(dog)
-    }
-  }
-
-  async delete(id: any) {
-    this.dogs = this.dogs.filter((dog) => !dog.id.equals(id))
-  }
-}
-
-test.group('UpdateRobotDogUseCase', () => {
   test('should update the name successfully', async ({ assert }) => {
-    const fakeRepo = new FakeRobotDogRepository()
     const dog = RobotDog.create('SN-001', 'Rex', 80)
     await fakeRepo.save(dog)
 
-    const useCase = new UpdateRobotDogUseCase(fakeRepo)
     await useCase.execute({ id: dog.id.value, name: 'Bolt' })
 
     const updated = await fakeRepo.findById(dog.id)
@@ -46,11 +26,9 @@ test.group('UpdateRobotDogUseCase', () => {
   })
 
   test('should throw RobotDogNotFoundError if robot does not exist', async ({ assert }) => {
-    const fakeRepo = new FakeRobotDogRepository()
-    const useCase = new UpdateRobotDogUseCase(fakeRepo)
-
-    await assert.rejects(async () => {
-      await useCase.execute({ id: 'non-existent-id', name: 'Bolt' })
-    }, RobotDogNotFoundError )
+    await assert.rejects(
+      async () => await useCase.execute({ id: 'non-existent-id', name: 'Bolt' }),
+      RobotDogNotFoundError
+    )
   })
 })
