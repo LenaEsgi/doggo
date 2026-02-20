8bf5febdb9615f5d5fde6c35e2d13c5584f1c45b
Fantom <arthur.morelon@gmail.com>
2026-02-20 15:29:38 +0100

    add log for index-robot-dog

--- app/modules/dogs/application/usecases/index-robot-dogs.use-case.implementation.ts
+++ app/modules/dogs/application/usecases/index-robot-dogs.use-case.implementation.ts
@@ -2,13 +2,18 @@ import { RobotDogRepository } from '../../domain/contracts/robot-dog.repository.
 import { IndexRobotDogsUseCase } from '../contracts/index-robot-dogs.use-case.js'
 import { RobotDogOutput } from '../DTO/robot-dog.output.dto.js'
 import { inject } from '@adonisjs/core'
+import logger from '@adonisjs/core/services/logger'
 
 @inject()
 export class IndexRobotDogsUseCaseImplementation implements IndexRobotDogsUseCase {
   constructor(private readonly robotDogRepository: RobotDogRepository) {}
 
   async execute(): Promise<RobotDogOutput[]> {
+    logger.info({}, 'IndexRobotDogsUseCase started')
+
     const dogs = await this.robotDogRepository.findAll()
+
+    logger.info({ count: dogs.length }, 'IndexRobotDogsUseCase completed successfully')
 
     return dogs.map((dog) => ({
       id: dog.id.value,
--- app/modules/dogs/infrastructure/http/controllers/index-robot-dog.controller.ts
+++ app/modules/dogs/infrastructure/http/controllers/index-robot-dog.controller.ts
@@ -6,16 +6,22 @@ import { HttpContext } from '@adonisjs/core/http'
 export default class ListRobotDogsController {
   constructor(private listRobotDogs: IndexRobotDogsUseCase) {}
 
-  public async handle({ response }: HttpContext) {
+  public async handle({ response, logger }: HttpContext) {
+    logger.info({}, 'ListRobotDogsController called')
+
     const robots = await this.listRobotDogs.execute()
 
-    return response.status(200).json(robots.map((dog) => ({
-      id: dog.id,
-      serialNumber: dog.serialNumber,
-      name: dog.name,
-      state: dog.state,
-      batteryLevel: dog.batteryLevel,
-      lastHeartbeat: dog.lastHeartbeat,
-    })))
+    logger.info({ count: robots.length }, 'ListRobotDogsController completed successfully')
+
+    return response.status(200).json(
+      robots.map((dog) => ({
+        id: dog.id,
+        serialNumber: dog.serialNumber,
+        name: dog.name,
+        state: dog.state,
+        batteryLevel: dog.batteryLevel,
+        lastHeartbeat: dog.lastHeartbeat,
+      }))
+    )
   }
 }
