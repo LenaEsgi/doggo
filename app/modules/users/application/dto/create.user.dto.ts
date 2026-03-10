60e3e3019fc195745be149ca438f3d54c3218902
lenaaaaaaa <vincennes.lena@gmail.com>
2026-03-10 15:12:22 +0100

    remove create user in module user

--- app/modules/users/application/dto/create.user.dto.ts
+++
@@ -1,5 +0,0 @@
-export type CreateUserDto = {
-  email: string
-  firstname: string
-  lastname: string
-}
--- app/modules/users/domain/contracts/user.write.repository.ts
+++ app/modules/users/domain/contracts/user.write.repository.ts
@@ -1,7 +1,6 @@
 import { type User } from '#users/domain/user.entity'
 
 export abstract class UserWriteRepository {
-  abstract create(user: User): Promise<User>
   abstract update(user: User): Promise<User>
   abstract delete(id: string): Promise<void>
 }
