bb54002e84b04353b6084808738ad5a0b24d1bbb
Fantom <arthur.morelon@gmail.com>
2026-04-14 19:34:54 +0200

    refactor: update abandon user dog controller to use specific body validator and adjust Docker CMD

--- app/modules/users/infrastructure/http/controllers/abandon.user.dog.controller.ts
+++ app/modules/users/infrastructure/http/controllers/abandon.user.dog.controller.ts
@@ -2,7 +2,7 @@ import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
 import { AbandonRobotDogUseCase } from '#app/modules/users/ownerships/application/usecases/abandon-robot-dog.use-case'
 import {
-  manageUserDogsBodyValidator,
+  manageUserDogsBodyValidatorForAbandon,
   manageUserDogsParamsValidator,
 } from '#users/infrastructure/http/validators/manage.user.dogs.validator'
 
@@ -14,7 +14,7 @@ export default class AbandonUserDogController {
     const { id } = await request.validateUsing(manageUserDogsParamsValidator, {
       data: request.params(),
     })
-    const { robotDogId } = await request.validateUsing(manageUserDogsBodyValidator)
+    const { robotDogId } = await request.validateUsing(manageUserDogsBodyValidatorForAbandon)
 
     logger.info({ userId: id, robotDogId }, 'AbandonUserDogController called')
     await this.useCase.execute(id, robotDogId)
