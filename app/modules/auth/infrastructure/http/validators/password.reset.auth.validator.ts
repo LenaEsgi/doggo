b05e45faa4b1a6c1b0c55762a16d1c88634d63d7
Fantom <arthur.morelon@gmail.com>
2026-04-15 22:03:08 +0200

    refactor: remove unnecessary email normalization from authentication validators

--- app/modules/auth/infrastructure/http/validators/password.reset.auth.validator.ts
+++ app/modules/auth/infrastructure/http/validators/password.reset.auth.validator.ts
@@ -2,6 +2,6 @@ import vine from '@vinejs/vine'
 
 export const passwordResetAuthValidator = vine.compile(
   vine.object({
-    email: vine.string().trim().email().normalizeEmail(),
+    email: vine.string().trim().email(),
   })
 )
--- app/modules/auth/infrastructure/http/validators/register.auth.validator.ts
+++ app/modules/auth/infrastructure/http/validators/register.auth.validator.ts
@@ -4,7 +4,7 @@ export const registerAuthValidator = vine.compile(
   vine.object({
     firstname: vine.string().trim().minLength(2).maxLength(100),
     lastname: vine.string().trim().minLength(2).maxLength(100),
-    email: vine.string().trim().email().normalizeEmail(),
+    email: vine.string().trim().email(),
     password: vine.string().minLength(8).maxLength(128),
   })
 )
