10f9c617b87f15c66fec3bf257d02f71c920f4d1
Fantom <arthur.morelon@gmail.com>
2026-05-27 20:15:12 +0200

    refactor: rename routes.ts to routes.v1.ts in all modules

---
+++ app/modules/dogs/infrastructure/http/routes.v1.ts
@@ -0,0 +1,21 @@
+import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+//import { middleware } from '#start/kernel'
+const CreateRobotDogController = () => import('./controllers/create-robot-dog.controller.js')
+const DestroyRobotDogController = () => import('./controllers/destroy-robot-dog.controller.js')
+const ShowRobotDogController = () => import('./controllers/show-robot-dog.controller.js')
+const IndexRobotDogController = () => import('./controllers/index-robot-dog.controller.js')
+const UpdateRobotDogController = () => import('./controllers/update-robot-dog.controller.js')
+const ListUserRobotDogsController = () => import('./controllers/list-user-robot-dogs.controller.js')
+
+router
+  .group(() => {
+    router.get('/', [IndexRobotDogController])
+    router.get('/users/:id', [ListUserRobotDogsController])
+    router.get('/:id', [ShowRobotDogController])
+    router.post('/', [CreateRobotDogController])
+    router.delete('/:id', [DestroyRobotDogController])
+    router.put('/:id', [UpdateRobotDogController])
+  })
+  .prefix('/dogs')
+  .use(middleware.firebaseAuth())
---
+++ app/modules/missions/infrastructure/http/routes.v1.ts
@@ -0,0 +1,52 @@
+import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+
+const CreateMissionController = () =>
+  import('#app/modules/missions/infrastructure/http/controllers/create-mission.controller')
+const ShowMissionController = () =>
+  import('#app/modules/missions/infrastructure/http/controllers/show-mission.controller')
+const IndexMissionController = () =>
+  import('#app/modules/missions/infrastructure/http/controllers/index-mission.controller')
+const UpdateMissionController = () =>
+  import('#app/modules/missions/infrastructure/http/controllers/update-mission.controller')
+const DestroyMissionController = () =>
+  import('#app/modules/missions/infrastructure/http/controllers/destroy-mission.controller')
+const AddStepController = () =>
+  import('#app/modules/missions/infrastructure/http/controllers/add-step.controller')
+const DestroyMissionStepController = () =>
+  import('#app/modules/missions/infrastructure/http/controllers/destroy-mission-step.controller')
+
+const MoveMissionStepController = () =>
+  import('#app/modules/missions/infrastructure/http/controllers/move-mission-step.controller')
+
+const ListMissionsByDogUseCase = () =>
+  import('#app/modules/missions/infrastructure/http/controllers/list-missions-by-dog.controller')
+
+const AssignToDogController = () =>
+  import('#app/modules/missions/infrastructure/http/controllers/assign-to-dog.controller')
+
+const RemoveFromDogController = () =>
+  import('#app/modules/missions/infrastructure/http/controllers/remove-from-dog.controller')
+
+router
+  .group(() => {
+    router.post('/', [CreateMissionController])
+    router.get('/', [IndexMissionController])
+    router.get('/:id', [ShowMissionController])
+    router.put('/:id', [UpdateMissionController])
+    router.delete('/:id', [DestroyMissionController])
+
+    router.post('/:id/steps', [AddStepController])
+    router.delete('/:missionId/steps/:stepId', [DestroyMissionStepController])
+    router.put('/:missionId/steps/:stepId', [MoveMissionStepController])
+  })
+  .prefix('/missions')
+  .use(middleware.firebaseAuth())
+
+router
+  .group(() => {
+    router.get('/dogs/:id/missions', [ListMissionsByDogUseCase])
+    router.post('/dogs/:id/assign', [AssignToDogController])
+    router.delete('/dogs/:id/missions/:missionId', [RemoveFromDogController])
+  })
+  .use(middleware.firebaseAuth())
