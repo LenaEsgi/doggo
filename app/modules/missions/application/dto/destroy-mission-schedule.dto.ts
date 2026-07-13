ad21b6057ee0d91e8af063d05037c4b648e166fe
Fantom <arthur.morelon@gmail.com>
2026-07-13 15:08:35 +0200

    feat: add DestroyMissionScheduleUseCase

---
+++ app/modules/missions/application/dto/destroy-mission-schedule.dto.ts
@@ -0,0 +1,3 @@
+export class DestroyMissionScheduleDto {
+  constructor(public readonly id: string) {}
+}
---
+++ app/modules/missions/application/usecases/destroy-mission-schedule.use-case.ts
@@ -0,0 +1,24 @@
+import { inject } from '@adonisjs/core'
+import logger from '@adonisjs/core/services/logger'
+import { MissionScheduleRepository } from '#app/modules/missions/domain/contracts/mission-schedule.repository'
+import { DestroyMissionScheduleDto } from '#app/modules/missions/application/dto/destroy-mission-schedule.dto'
+import { MissionScheduleId } from '#app/modules/missions/domain/value-objects/mission-schedule-id'
+import { MissionScheduleNotFoundError } from '#app/modules/missions/domain/exceptions/mission-schedule-not-found.error'
+
+@inject()
+export class DestroyMissionScheduleUseCase {
+  constructor(private missionScheduleRepository: MissionScheduleRepository) {}
+
+  async execute(dto: DestroyMissionScheduleDto): Promise<void> {
+    logger.info('DestroyMissionScheduleUseCase started', { dto })
+
+    const scheduleId = MissionScheduleId.fromString(dto.id)
+    const schedule = await this.missionScheduleRepository.findById(scheduleId)
+
+    if (!schedule) {
+      throw new MissionScheduleNotFoundError(dto.id)
+    }
+
+    await this.missionScheduleRepository.delete(schedule.id)
+  }
+}
